70bc82ab1643d4907e263174d3ca05051126dd12
Aljaž Mur Eržen <aljaz@edgedb.com>
2024-05-09 15:59:18 +0200

    organize

--- flake.nix
+++ flake.nix
@@ -16,20 +16,13 @@
   };
 
   outputs = inputs@{ flake-parts, nixpkgs, crane, fenix, ... }:
-    let
-      # Set version numbers as variables
-      edgedbServerVersion = {
-        major = 5;
-        minor = 3;
-      };
-      edgedbCliVersion = "v5.1.0";
-    in flake-parts.lib.mkFlake { inherit inputs; } {
+    flake-parts.lib.mkFlake { inherit inputs; } {
       systems =
         [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
       perSystem = { config, system, ... }:
         let
           pkgs = nixpkgs.legacyPackages.${system};
-          mk_edgedb_server = { source }:
+          mk_edgedb_server = source:
             pkgs.stdenvNoCC.mkDerivation {
               name = "edgedb-server";
               buildInputs = [ ];
@@ -44,6 +37,31 @@
                 cp -r ./* $out
               '';
             };
+
+          fetchEdgeDBServer = { platform, version }:
+            let
+              jsonURL =
+                "https://packages.edgedb.com/archive/.jsonindexes/${platform}.json";
+              jsonFile = builtins.fetchurl jsonURL;
+              json = builtins.fromJSON (builtins.readFile jsonFile);
+              package = builtins.head (builtins.filter (p:
+                p.basename == "edgedb-server" && p.version_details.major
+                == version.major && p.version_details.minor == version.minor)
+                json.packages);
+              installRef = builtins.head
+                (builtins.filter (i: i.encoding == "zstd") package.installrefs);
+            in {
+              url = "https://packages.edgedb.com" + installRef.ref;
+              sha256 = installRef.verification.sha256;
+            };
+
+          edgedbPlatform = {
+            x86_64-linux = "x86_64-unknown-linux-gnu";
+            aarch64-linux = "aarch64-unknown-linux-gnu";
+            x86_64-darwin = "x86_64-apple-darwin";
+            aarch64-darwin = "aarch64-apple-darwin";
+          }.${system};
+
           mk_edgedb_cli = { source }:
             (let
               inherit (pkgs) lib;
@@ -72,35 +90,18 @@
               cargoArtifacts = craneLib.buildDepsOnly commonArgs;
             in craneLib.buildPackage
             (commonArgs // { inherit cargoArtifacts; }));
-          fetchEdgeDBServer = system:
-            let
-              jsonURL =
-                "https://packages.edgedb.com/archive/.jsonindexes/${system}.json";
-              jsonFile = builtins.fetchurl jsonURL;
-              json = builtins.fromJSON (builtins.readFile jsonFile);
-              package = builtins.head (builtins.filter (p:
-                p.basename == "edgedb-server" && p.version_details.major
-                == edgedbServerVersion.major && p.version_details.minor
-                == edgedbServerVersion.minor) json.packages);
-              installRef = builtins.head
-                (builtins.filter (i: i.encoding == "zstd") package.installrefs);
-            in {
-              url = "https://packages.edgedb.com" + installRef.ref;
-              sha256 = installRef.verification.sha256;
-            };
         in {
-          packages.edgedb-server = mk_edgedb_server {
-            source = {
-              x86_64-linux = fetchEdgeDBServer "x86_64-unknown-linux-gnu";
-              aarch64-linux = fetchEdgeDBServer "aarch64-unknown-linux-gnu";
-              x86_64-darwin = fetchEdgeDBServer "x86_64-apple-darwin";
-              aarch64-darwin = fetchEdgeDBServer "aarch64-apple-darwin";
-            }.${system};
-          };
+          packages.edgedb-server = mk_edgedb_server (fetchEdgeDBServer {
+            platform = edgedbPlatform;
+            version = {
+              major = 5;
+              minor = 3;
+            };
+          });
           packages.edgedb-cli = mk_edgedb_cli {
             source = {
               url = "https://github.com/edgedb/edgedb-cli";
-              rev = edgedbCliVersion;
+              rev = "v5.1.0";
               hash = "sha256-znxAtfSeepLQqkPsEzQBp3INZym5BLap6m29C/9z+h8=";
             };
           };
